f604cc6c6efe1eb6d4b3facf5bf70ae79c3df495
Cindy Carrillo <ccarrillog2@miumg.edu.gt>
2026-03-22 21:51:51 -0600

    Add files via upload

---
+++ problema1 Control de calificaciones.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Cindy Julissa Carrillo Guarcas" << endl << endl;
+    float nota;
+    float suma = 0;
+    int Aprobadas = 0;
+    int Reprobadas = 0;
+    int totalNotas = 5;
+
+    for(int i = 1; i <= totalNotas; i++) {
+        cout << "Ingrese la nota" << i << ": ";
+        cin >> nota;
+
+        suma += nota;
+
+        if(nota >= 61) {
+            Aprobadas++;
+        } else {
+            Reprobadas++;
+        }
+    }
+
+    float promedio = suma / totalNotas;
+
+    cout << "Suma total: " << suma << endl;
+    cout << "Promedio: " << promedio << endl;
+    cout << "Notas aprobadas: " << Aprobadas << endl;
+    cout << "Notas reprobadas: " << Reprobadas << endl;
+
+    if(promedio >= 61) {
+        cout << "Resultado final: Aprueba." << endl;
+    } else {
+        cout << "Resultado final: Reprueba" << endl;
+    }
+    return 0;
+}
